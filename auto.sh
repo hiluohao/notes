6c428bab401ecb1220f323791c9e0dc667d7d538
luohao <197625807@qq.com>
2018-12-09 13:51:54 +0800

    “update”

---
+++ auto.sh
@@ -0,0 +1,12 @@
+echo .....Jump to folder C:\Users\Administrator\Documents\pages\github\hiluohao
+
+echo .....!!!!git checkout master
+
+git checkout master
+
+hexo clean
+hexo g
+hexo d
+
+echo .....enter any key to exit
+
