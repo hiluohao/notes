1fc9eeb7a22fe5990efd7a1835edf2dae0a07b70
luohao <197625807@qq.com>
2018-12-09 15:01:28 +0800

    “update”

--- auto.sh
+++ auto.sh
@@ -1,12 +1,9 @@
-echo .....Jump to folder C:\Users\Administrator\Documents\pages\github\hiluohao
-
-echo .....!!!!git checkout master
-
-git checkout master
-
 hexo clean
 hexo g
 hexo d
 
-echo .....enter any key to exit
+git add .
+git commit -m “update”
+git push origin pages
 
+echo .....enter any key to exit
